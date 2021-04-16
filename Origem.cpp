16c9d21a977974fc69501cdf163f17ae33c1fd1b
Juliano Maia <julianosam@pop-os.localdomain>
2021-04-16 01:04:35 -0400

    Adding new functionality

--- Origem.cpp
+++ Origem.cpp
@@ -7,6 +7,9 @@ int menu();
 void InserirMusica();
 void ListarMusicas();
 void BuscarMusicas();
+void Sair();
+void IncializarAcervo();
+void InsereItemNaLista(struct Musica musica);
 
 struct Musica {
   char titulo[50];
@@ -24,6 +27,9 @@ int main(int argc, char const *argv[])
 
   Head = NULL;
 
+
+  IncializarAcervo();
+
   while(1)
   {
     opcao = menu();
@@ -38,6 +44,9 @@ int main(int argc, char const *argv[])
       break;
     case 3:
       BuscarMusicas();
+      break;
+    case 4:
+      Sair();
       break;
     default:
       printf("Opção inválida.");
@@ -76,7 +85,12 @@ void InserirMusica()
   Musica *NovaMusica;
   NovaMusica = (struct Musica *)malloc(sizeof(struct Musica));
   Musica *ElementoVarredura;
-  ElementoVarredura = (struct Musica *)malloc(sizeof(struct Musica));
+  
+  // you dont neeed to allocate memory for this since this is just a pointer
+  // this is actually going to cause a "memory leak", since this will not be referenced
+  // by any variable after this function returns, you will never be able to free() this memory
+  // again. 
+  // ElementoVarredura = (struct Musica *)malloc(sizeof(struct Musica));
 
   printf("\nDados da música a ser inserida: ");
   printf("\nTítulo: ");
@@ -87,6 +101,9 @@ void InserirMusica()
   scanf("%f", &NovaMusica->duracao);
   while((c = getchar()) != '\n' && c != EOF) {} //limpar o buffer
 
+
+  // you can change this function to use the InsereItemNaLista() too if you want
+  // instead of keeping this code duplicated here.
   if(Head == NULL)
   {
     Head = NovaMusica;
@@ -109,32 +126,29 @@ void BuscarMusicas()
   int c;
 
   Musica *ElementoVarredura;
-  ElementoVarredura = (struct Musica *)malloc(sizeof(struct Musica));
-
-  printf("Listar Músicas.\n");
 
   printf("Digite o título da música a ser pesquisado: ");
   fgets(tituloBuscado, 49, stdin);
   // while((c = getchar()) != '\n' && c != EOF) {} //limpar o buffer
 
   ElementoVarredura = Head;
-  if(ElementoVarredura == NULL)
-  {
-    getchar();
-    return;
-  }
+
   while (ElementoVarredura != NULL)
   {
-    if(ElementoVarredura->titulo == tituloBuscado)
-    {
+    
+    // when comparing strings, the == actually compares the memory addresses that
+    // the strings (pointers) are stored, not actually the content of the strings
+    // you need to use a function like strcmp() that actually will de-reference each char of the string
+    // and compare one by one
+    // if(ElementoVarredura->titulo == tituloBuscado) {
+    if( strcmp(ElementoVarredura->titulo, tituloBuscado) == 0 ) {
       printf("Título: %s \n", ElementoVarredura->titulo);
       printf("Artista: %s \n", ElementoVarredura->artista);
       printf("Duração: %2.2fmin \n", ElementoVarredura->duracao);
     }
-    else
-    {
-      ElementoVarredura = ElementoVarredura->prox;
-    }
+
+    ElementoVarredura = ElementoVarredura->prox;
+
   }
   printf("\n");
 
@@ -145,24 +159,85 @@ void BuscarMusicas()
 void ListarMusicas()
 {
   
-  Musica *ElementoVarredura;
-  ElementoVarredura = (struct Musica *)malloc(sizeof(struct Musica));
+  // starts pointing to the head of the list
+  // since ElementoVarredura is a pointer, you don't need to allocate memory for this
+  // it's just pointing to a memory address where the struct is stored, not actually storing the struct
+  Musica *ElementoVarredura = Head;
 
-  ElementoVarredura = Head;
-  if(ElementoVarredura == NULL)
-  {
-    return;
-  }
+  // while there's items in the list... 
+  // note that if list is empty it won't even enter the while loop
   while (ElementoVarredura != NULL)
   {
+    printf("\n####################################\n");
     printf("Título: %s", ElementoVarredura->titulo);
     printf("Artista: %s", ElementoVarredura->artista);
     printf("Duração: %2.2f", ElementoVarredura->duracao);
     ElementoVarredura = ElementoVarredura->prox;
   }
   printf("\n");
-
   getchar();
   return;
   
+}
+
+
+void IncializarAcervo() {
+
+  // this is a local allocated array, meaning when this function returns, the memory allocated for
+  // the acervo[] array is lost.
+  struct Musica acervo[] = {
+    { "Living on a Prayer\n", "Bon Jovi\n", 3.00, NULL},
+    { "Vai Lacraia\n", "MC Serginho\n", 3.00, NULL},
+    { "Faroeste Caboclo\n", "Legiao Urbana\n", 3.00, NULL},
+    { "O Tikaco eh foda\n", "Bloco Tikaco\n", 3.00, NULL},
+    { "Boate Azul\n", "Nao Lembro quem canta\n", 3.00, NULL}
+  };
+  // Dont forget to update the size here if you add more songs.
+  // there are ways to calculate the size of the array automatically,
+  // you can google it :)
+  unsigned int tamanhoAcervo = 5;
+
+  for(int i = 0; i < tamanhoAcervo; i++) {
+    
+    InsereItemNaLista(acervo[i]);
+
+  }
+  
+}
+
+void InsereItemNaLista(struct Musica musica) {
+
+  // here we need to allocate memory because we're copying data from the local array "acervo",
+  // into the linked list
+  struct Musica* NovaMusica = (struct Musica *) malloc(sizeof(struct Musica));
+  // note that NovaMusica->titulo = musica.titulo won't work, because you would be simply
+  // making the NovaMusica pointer point to the string in the musica struct, which is locally defined
+  // in the other function and would be lost after the function returns.
+  // we need to use strcpy() which copies the string, char by char, onto the other string.
+  strcpy(NovaMusica->titulo, musica.titulo);
+  strcpy(NovaMusica->artista, musica.artista);
+  NovaMusica->duracao = musica.duracao;
+  NovaMusica->prox = NULL;
+
+  if(Head == NULL)
+  {
+    Head = NovaMusica;
+    Head->prox = NULL;
+  }
+  else
+  {
+    struct Musica* ElementoVarredura = Head;
+    while (ElementoVarredura->prox != NULL)
+      ElementoVarredura = ElementoVarredura->prox;
+
+    ElementoVarredura->prox = NovaMusica;
+    NovaMusica->prox = NULL;
+  }
+
+}
+
+
+void Sair() {
+  printf("Volte sempre! :)\n");
+  exit(0);
 }
